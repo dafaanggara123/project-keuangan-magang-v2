7daccde4b66121f967ea7f69cff8965b5200ea84
dafa <dafaanggara011@gmail.com>
2026-09-20 08:32:30 +0700

    prepare project for vercel

--- backend/server.js
+++ backend/server.js
@@ -3269,14 +3269,4 @@ app.use(
    START SERVER
    ========================================================= */
 
-app.listen(
-  PORT,
-  '0.0.0.0',
-  () => {
-
-    console.log(
-      `Dapoersari Project 2 backend berjalan di http://localhost:${PORT}`
-    );
-
-  }
-);
+export default app;
---
+++ backend/start.js
@@ -0,0 +1,9 @@
+import app from './server.js';
+
+const PORT = process.env.PORT || 4100;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(
+    `Dapoersari Project 2 backend berjalan di http://localhost:${PORT}`
+  );
+});
